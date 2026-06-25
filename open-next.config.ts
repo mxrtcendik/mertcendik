d786ec6213fbc9185390c5289715166090712c49
Mert Cendik <mert@mertcendik.dev>
2026-06-25 13:34:25 +0300

    cloudflare-env.d.ts, open-next.config.ts, wrangler.jsonc removed from gitignore

---
+++ open-next.config.ts
@@ -0,0 +1,10 @@
+// default open-next.config.ts file created by @opennextjs/cloudflare
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
+
+export default defineCloudflareConfig({
+	incrementalCache: withRegionalCache(r2IncrementalCache, {
+		mode: "long-lived",
+	}),
+});
